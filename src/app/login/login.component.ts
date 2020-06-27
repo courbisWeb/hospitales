7a6deeda50d33dd6103685da0e74c1df7dfa98e1
Julio Courbis <jcourbis@amisoft.cl>
2020-06-27 16:05:54 -0400

    Sección 13 terminada

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,67 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { NgForm } from '@angular/forms';
+import { UsuarioService } from '../services/service.index';
+import { Usuario } from '../models/usuario.model';
+
+declare function init_plugins();
+declare const gapi: any;
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  public recuerdame: boolean = false;
+  public email: string;
+  auth2: any;
+
+  constructor(
+    public router: Router,
+    public _usuarioService: UsuarioService ) { }
+
+  ngOnInit() {
+    init_plugins();
+    this.googleInit();
+    this.email = localStorage.getItem('email') || '';
+    if ( this.email.length > 0 ) {
+      this.recuerdame = true;
+    }
+  }
+
+  googleInit() {
+    gapi.load('auth2', () => {
+      this.auth2 = gapi.auth2.init({
+        client_id: '308515534128-uu6vk7p53t85qel4igtctbecut02em72.apps.googleusercontent.com',
+        cookiepolicy: 'single_host_origin',
+        scope: 'profile email'
+      });
+      this.attachSignin( document.getElementById('btnGoogle') );
+    });
+  }
+
+  attachSignin( element ) {
+
+    this.auth2.attachClickHandler( element, {}, (googleUser) => {
+      //let profile = googleUser.getBasicProfile();
+      let token = googleUser.getAuthResponse().id_token;
+
+      this._usuarioService.loginGoogle( token ).subscribe(correcto => window.location.href='#dashboard');
+    });
+
+  }
+
+  ingresar(forma: NgForm) {
+
+    if ( !forma.valid ) {
+      return;
+    }
+
+    let usuario = new Usuario(null, forma.value.email, forma.value.password);
+    this._usuarioService.login( usuario, forma.value.recuerdame ).subscribe(correcto => window.location.href='#dashboard');
+
+  }
+
+}
