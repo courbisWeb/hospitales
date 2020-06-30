2016ccbd15d6fdf901221237e7e307efec65e298
Julio Courbis <jcourbis@amisoft.cl>
2020-06-30 17:56:11 -0400

    Sección 14 lista

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,6 +1,7 @@
 
 import { NgModule } from '@angular/core';
 import { PAGES_ROUTES } from './pages.routes';
+import { CommonModule } from '@angular/common';
 
 import { SharedModule } from '../shared/shared.module';
 
@@ -16,6 +17,8 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
 import { Graficas1Component } from './graficas1/graficas1.component';
 
+//Pipe module
+import { PipesModule } from '../pipes/pipes.module';
 
 // temporal
 import { IncrementadorComponent } from '../components/incrementador/incrementador.component';
@@ -23,6 +26,7 @@ import { GraficoDonaComponent } from '../components/grafico-dona/grafico-dona.co
 import { AccoutSettingsComponent } from './accout-settings/accout-settings.component';
 import { PromesasComponent } from './promesas/promesas.component';
 import { RxjsComponent } from './rxjs/rxjs.component';
+import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
     declarations: [
@@ -34,7 +38,8 @@ import { RxjsComponent } from './rxjs/rxjs.component';
         GraficoDonaComponent,
         AccoutSettingsComponent,
         PromesasComponent,
-        RxjsComponent
+        RxjsComponent,
+        ProfileComponent
     ],
     exports: [
         DashboardComponent,
@@ -45,7 +50,9 @@ import { RxjsComponent } from './rxjs/rxjs.component';
         SharedModule,
         PAGES_ROUTES,
         FormsModule,
-        ChartsModule
+        ChartsModule,
+        PipesModule,
+        CommonModule
     ]
 })
 export class PagesModule { }
--- src/app/services/usuario/usuario.service.ts
+++ src/app/services/usuario/usuario.service.ts
@@ -5,6 +5,7 @@ import { URL_SERVICIOS } from 'src/app/config/config';
 import {map} from 'rxjs/operators';
 import swal from 'sweetalert';
 import { Router } from '@angular/router';
+import { SubirArchivoService } from '../subir-archivo/subir-archivo.service';
 
 @Injectable({
   providedIn: 'root'
@@ -16,7 +17,8 @@ export class UsuarioService {
 
   constructor(
     public http: HttpClient,
-    public router: Router
+    public router: Router,
+    public _subirArchivoService: SubirArchivoService
   ) {
     this.cargarStorage();
   }
@@ -89,6 +91,29 @@ export class UsuarioService {
       return resp.usuario;
     }));
 
+  }
+
+  actualizarUsuario( usuario: Usuario ) {
+    let url = URL_SERVICIOS + '/usuario/' + usuario._id;
+    url += '?token=' + this.token;
+    return this.http.put(url, usuario)
+    .pipe(map( (resp: any) => {
+      this.guardarStorage(resp.usuario._id, this.token, resp.usuario);
+      swal('Usuario actualizado ', usuario.nombre, 'success');
+      return true;
+    }));
+  }
+
+  cambiarImagen( archivo: File, id: string ) {
+    this._subirArchivoService.subirArchivo(archivo, 'usuarios', id)
+      .then( (resp: any) => {
+        this.usuario.img = resp.usuarioActualizado.img;
+        swal('Imagen actualizada', resp.usuarioActualizado.nombre, 'success');
+        this.guardarStorage(id, this.token, resp.usuarioActualizado);
+      })
+      .catch( resp => {
+        console.log(resp);
+      })
   }
 
 }
